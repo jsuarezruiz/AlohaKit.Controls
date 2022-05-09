ed36afed1a36b093918fc35449bc0c0c38b08326
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2022-05-09 19:41:32 +0200

    Updated sample adding settings parameters to some views

--- src/AlohaKit.Gallery/Views/CheckBoxView.xaml.cs
+++ src/AlohaKit.Gallery/Views/CheckBoxView.xaml.cs
@@ -5,5 +5,67 @@ public partial class CheckBoxView : ContentPage
 	public CheckBoxView()
 	{
 		InitializeComponent();
-	}
+        
+        UpdateColors();
+    }
+
+    void OnCheckedColorEntryTextChanged(object sender, TextChangedEventArgs e)
+    {
+        UpdateColors();
+    }
+
+    void OnUncheckedColorEntryTextChanged(object sender, TextChangedEventArgs e)
+    {
+        UpdateColors();
+    }
+
+    void OnStrokeColorEntryTextChanged(object sender, TextChangedEventArgs e)
+    {
+        UpdateColors();
+    }
+
+    void UpdateColors()
+    {
+        var checkedColor = GetColorFromString(CheckedColorEntry.Text);
+
+        if (checkedColor != null)
+        {
+            CheckedColorEntry.BackgroundColor = checkedColor;
+
+            CheckBox.CheckedBrush = new SolidColorBrush(checkedColor);
+        }
+
+        var uncheckedColor = GetColorFromString(UncheckedColorEntry.Text);
+
+        if (uncheckedColor != null)
+        {
+            UncheckedColorEntry.BackgroundColor = uncheckedColor;
+
+            CheckBox.UncheckedBrush = new SolidColorBrush(uncheckedColor);
+        }
+
+        var strokeColor = GetColorFromString(StrokeColorEntry.Text);
+
+        if (uncheckedColor != null)
+        {
+            StrokeColorEntry.BackgroundColor = strokeColor;
+
+            CheckBox.Stroke = new SolidColorBrush(strokeColor);
+        }
+    }
+
+    Color GetColorFromString(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+            return null;
+
+        try
+        {
+            return Color.FromArgb(value[0].Equals('#') ? value : $"#{value}");
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+    }
 }
--- src/AlohaKit.Gallery/Views/PulseIconView.xaml.cs
+++ src/AlohaKit.Gallery/Views/PulseIconView.xaml.cs
@@ -5,5 +5,50 @@ public partial class PulseIconView : ContentPage
 	public PulseIconView()
 	{
 		InitializeComponent();
-	}
+
+        UpdateColors();
+    }
+
+    void OnPulseColorEntryTextChanged(object sender, TextChangedEventArgs e)
+    {
+        UpdateColors();
+    }
+
+    void OnBackgroundColorEntryTextChanged(object sender, TextChangedEventArgs e)
+    {
+        UpdateColors();
+    }
+
+    void UpdateColors()
+    {
+        var backgroundColor = GetColorFromString(BackgroundColorEntry.Text);
+
+        if (backgroundColor != null)
+        {
+            BackgroundColorEntry.BackgroundColor = backgroundColor;
+            PulseIcon.Background = new SolidColorBrush(backgroundColor);
+        }
+
+        var pulseColor = GetColorFromString(PulseColorEntry.Text);
+
+        if (pulseColor != null)
+        {
+            PulseIcon.PulseColor = PulseColorEntry.BackgroundColor = pulseColor;
+        }
+    }
+
+    Color GetColorFromString(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+            return null;
+
+        try
+        {
+            return Color.FromArgb(value[0].Equals('#') ? value : $"#{value}");
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+    }
 }
