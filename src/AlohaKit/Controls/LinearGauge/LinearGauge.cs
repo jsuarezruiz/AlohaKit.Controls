348d1e66fea95deb2a0ac7156b7a2b6063752dad
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2022-08-20 18:33:09 +0200

    Added LinearGauge

---
+++ src/AlohaKit/Controls/LinearGauge/LinearGauge.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.Maui.Platform;
+
+namespace AlohaKit.Controls
+{    
+    // TODO: Include the Orientation property 
+    public class LinearGauge : GraphicsView
+    {
+        public LinearGauge()
+        {
+            HeightRequest = 200;
+            WidthRequest = 60;
+
+            Drawable = LinearGaugeDrawable = new LinearGaugeDrawable();
+        }
+
+        public LinearGaugeDrawable LinearGaugeDrawable { get; set; }
+       
+        public static readonly new BindableProperty BackgroundProperty =  
+            BindableProperty.Create(nameof(Background), typeof(Brush), typeof(Button), null,
+                propertyChanged: (bindableObject, oldValue, newValue) =>
+                {
+                    if (newValue != null && bindableObject is LinearGauge linearGauge)
+                    {
+                        linearGauge.UpdateBackground();
+                    }
+                });
+
+        public new Brush Background
+        {
+            get => (Brush)GetValue(BackgroundProperty);
+            set => SetValue(BackgroundProperty, value);
+        }
+
+        public static readonly BindableProperty RangeStartProperty =
+           BindableProperty.Create(nameof(RangeStart), typeof(int), typeof(LinearGauge), 0,
+               propertyChanged: (bindableObject, oldValue, newValue) =>
+               {
+                   if (newValue != null && bindableObject is LinearGauge linearGauge)
+                   {
+                       linearGauge.UpdateBackground();
+                   }
+               });
+
+        public int RangeStart
+        {
+            get => (int)GetValue(RangeStartProperty);
+            set => SetValue(RangeStartProperty, value);
+        }
+
+        public static readonly BindableProperty RangeEndProperty = 
+            BindableProperty.Create(nameof(RangeEnd), typeof(int), typeof(LinearGauge), 100,
+                propertyChanged: (bindableObject, oldValue, newValue) =>
+                {
+                    if (newValue != null && bindableObject is LinearGauge linearGauge)
+                    {
+                        linearGauge.UpdateRangeEnd();
+                    }
+                });
+
+        public int RangeEnd
+        {
+            get => (int)GetValue(RangeEndProperty);
+            set => SetValue(RangeEndProperty, value);
+        }
+
+        public static readonly BindableProperty ValueProperty =
+            BindableProperty.Create(nameof(Value), typeof(int), typeof(LinearGauge), 0,
+                propertyChanged: (bindableObject, oldValue, newValue) =>
+                {
+                    if (newValue != null && bindableObject is LinearGauge linearGauge)
+                    {
+                        linearGauge.UpdateValue();
+                        linearGauge.ValueChanged?.Invoke(linearGauge, new ValueChangedEventArgs((double)oldValue, (double)newValue));
+                    }
+                });
+
+        public int Value
+        {
+            get => (int)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        public static readonly BindableProperty CornerRadiusProperty =   
+            BindableProperty.Create(nameof(CornerRadius), typeof(CornerRadius), typeof(Button), new CornerRadius(),
+                propertyChanged: (bindableObject, oldValue, newValue) =>
+                {
+                    if (newValue != null && bindableObject is LinearGauge linearGauge)
+                    {
+                        linearGauge.UpdateCornerRadius();
+                    }
+                });
+
+        public CornerRadius CornerRadius
+        {
+            get => (CornerRadius)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+
+        public event EventHandler<ValueChangedEventArgs> ValueChanged;
+
+        protected override void OnParentSet()
+        {
+            base.OnParentSet();
+
+            if (Parent != null)
+            {
+                UpdateBackground();
+                UpdateRangeStart();
+                UpdateRangeEnd();
+                UpdateValue();
+                UpdateCornerRadius();
+            }
+        }
+
+        void UpdateBackground()
+        {
+            if (LinearGaugeDrawable == null)
+                return;
+
+            LinearGaugeDrawable.BackgroundPaint = Background;
+
+            Invalidate();
+        }
+
+        void UpdateRangeStart()
+        {
+            if (LinearGaugeDrawable == null)
+                return;
+
+            LinearGaugeDrawable.RangeStart = RangeStart;
+
+            Invalidate();
+        }
+
+        void UpdateRangeEnd()
+        {
+            if (LinearGaugeDrawable == null)
+                return;
+
+            LinearGaugeDrawable.RangeEnd = RangeEnd;
+
+            Invalidate();
+        }
+
+        void UpdateValue()
+        {
+            if (LinearGaugeDrawable == null)
+                return;
+
+            LinearGaugeDrawable.Value = Value;
+
+            Invalidate();
+        }
+
+        void UpdateCornerRadius()
+        {
+            if (LinearGaugeDrawable == null)
+                return;
+
+            LinearGaugeDrawable.CornerRadius = CornerRadius;
+
+            Invalidate();
+        }
+    }
+}
---
+++ src/AlohaKit/Controls/LinearGauge/LinearGaugeDrawable.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Maui.Graphics;
+
+namespace AlohaKit.Controls
+{
+	public class LinearGaugeDrawable : IDrawable
+	{
+		// TODO: Expose these properties 
+		Color Stroke = Colors.Black;
+		const float StrokeThickness = 2.0f;
+		const float TicksWidth = 20.0f;
+
+		public Paint BackgroundPaint { get; set; }
+		public int RangeStart { get; set; }
+		public int RangeEnd { get; set; }
+		public int Value { get; set; }
+		public CornerRadius CornerRadius { get; set; }
+
+		public void Draw(ICanvas canvas, RectF dirtyRect)
+		{
+			DrawBackground(canvas, dirtyRect);
+			DrawProgress(canvas, dirtyRect);
+			DrawTicks(canvas, dirtyRect);
+		}
+
+		void DrawBackground(ICanvas canvas, RectF dirtyRect)
+		{
+			canvas.SaveState();
+
+			canvas.StrokeColor = Stroke;
+			canvas.StrokeSize = StrokeThickness;
+
+			canvas.DrawRoundedRectangle(
+				dirtyRect.X + TicksWidth,
+				dirtyRect.Y,
+				dirtyRect.Width - TicksWidth,
+				dirtyRect.Height,
+				(float)CornerRadius.TopLeft,
+				(float)CornerRadius.TopRight,
+				(float)CornerRadius.BottomLeft,
+				(float)CornerRadius.BottomRight);
+
+			canvas.RestoreState();
+		}
+
+		void DrawProgress(ICanvas canvas, RectF dirtyRect)
+		{
+			if (BackgroundPaint != null)
+			{
+				canvas.SaveState();
+
+				int value = Value;
+
+				if (value > RangeEnd)
+					value = RangeEnd;
+
+				if (value < RangeStart)
+					value = RangeStart;
+
+				var percentage = (double)value / RangeEnd;
+				var progressHeight = dirtyRect.Height * percentage;
+				var progressY = dirtyRect.Y + dirtyRect.Height - progressHeight;
+
+				var rect = new Rect(
+					dirtyRect.X + TicksWidth + StrokeThickness / 2,
+					progressY + StrokeThickness / 2,
+					dirtyRect.Width - TicksWidth - StrokeThickness,
+					progressHeight - StrokeThickness);
+
+				canvas.SetFillPaint(BackgroundPaint, rect);
+
+				canvas.FillRoundedRectangle(
+					rect,
+					CornerRadius.TopLeft,
+					CornerRadius.TopRight,
+					CornerRadius.BottomLeft,
+					CornerRadius.BottomRight);
+
+				canvas.RestoreState();
+			}
+		}
+
+		void DrawTicks(ICanvas canvas, RectF dirtyRect)
+		{
+			int steps = 10;
+
+			for (int i = 0; i < steps; i++)
+			{
+				var stepScale = (double)i / steps;
+				Point nextLine = new Point(dirtyRect.X + TicksWidth, dirtyRect.Y + dirtyRect.Height * stepScale);
+
+				double defaultTickWidthh = 10.0d;
+				double tickWidth = defaultTickWidthh;
+
+				if (i != 0)
+				{
+					if (i == (steps / 2))
+						tickWidth = defaultTickWidthh * 2;
+
+					canvas.DrawLine(nextLine, nextLine.Offset(tickWidth, 0));
+
+					canvas.Font = Microsoft.Maui.Graphics.Font.Default;
+					var strValue = (int)(((double)RangeEnd / steps) * (steps - i));
+					PointF stringPosition = nextLine.Offset(-10, 0);
+
+					canvas.DrawString(strValue.ToString(), stringPosition.X, stringPosition.Y, HorizontalAlignment.Center);
+				}
+			}
+		}
+	}
+}
