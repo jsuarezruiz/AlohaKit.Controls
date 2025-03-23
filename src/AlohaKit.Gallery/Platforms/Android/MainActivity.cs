3ee89220a3ca78901b86fdaff7e750d8cbb4adaf
Javier Suárez <javiersuarezruiz@hotmail.com>
2025-03-23 16:24:22 +0100

    Updated to .NET 9

--- src/AlohaKit.Gallery/Platforms/Android/MainActivity.cs
+++ src/AlohaKit.Gallery/Platforms/Android/MainActivity.cs
@@ -4,8 +4,8 @@ using Android.OS;
 
 namespace AlohaKit.Gallery
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
-    public class MainActivity : MauiAppCompatActivity
-    {
-    }
+	[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+	public class MainActivity : MauiAppCompatActivity
+	{
+	}
 }
--- src/AlohaKit.Gallery/Platforms/iOS/AppDelegate.cs
+++ src/AlohaKit.Gallery/Platforms/iOS/AppDelegate.cs
@@ -2,9 +2,9 @@
 
 namespace AlohaKit.Gallery
 {
-    [Register("AppDelegate")]
-    public class AppDelegate : MauiUIApplicationDelegate
-    {
-        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-    }
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
 }
